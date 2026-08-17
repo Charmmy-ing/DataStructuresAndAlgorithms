0de6cddb8a54c1278fd4ca7d2f04e21fc8f0a154
Can_Lu <lc5207242@example.com>
2026-08-17 23:49:03 +0800

    8/17V1

---
+++ JavaCourse/src/JavaFoundationalCourse/JavaInnerClass.java
@@ -0,0 +1,15 @@
+package JavaFoundationalCourse;
+
+public class JavaInnerClass {
+    public static void main(String[] args) {
+        OutClass num1 = new OutClass();
+        OutClass.InnerClass num2 = num1.new InnerClass();
+        System.out.println(num1.a+num2.b);
+    }
+}
+ class OutClass{
+    int a=10;
+    class InnerClass{
+        int b=20;
+    }
+}
