8178c6e9de07c7495f5e51a380b4b7115c83a6bb
Can_Lu <lc5207242@example.com>
2026-08-18 23:42:21 +0800

    8/18v3

--- JavaCourse/src/JavaFoundationalCourse/JavaInnerClass.java
+++ JavaCourse/src/JavaFoundationalCourse/JavaInnerClass.java
@@ -13,17 +13,37 @@ package JavaFoundationalCourse;
 //        int b=20;
 //    }
 //}
+//------------
+//public class JavaInnerClass {
+//    public static void main(String[] args) {
+//        OutClass num1 = new OutClass();
+//        OutClass.InnerClass num2 = num1.new InnerClass();
+//        System.out.println(num1.a+"AND"+num2.InnerMethod());
+//    }
+//}
+//class OutClass{
+//    int a=10;
+//    class InnerClass{
+//        public int InnerMethod(){
+//            System.out.println(a++);
+//            return a;
+//
+//        }
+//    }
+//}
+//------------
 public class JavaInnerClass {
     public static void main(String[] args) {
         OutClass num1 = new OutClass();
-        OutClass.InnerClass num2 = num1.new InnerClass();
-        System.out.println(num1.a+"AND"+num2.InnerMethod());
+        OutClass.InnerClass num2 = new OutClass.InnerClass();
+        System.out.println(num1.b+"AND"+num2.InnerMethod());
     }
 }
 class OutClass{
-    int a=10;
-    class InnerClass{
+    int b=10;
+    static class InnerClass{
         public int InnerMethod(){
+            int a=10;
             System.out.println(a++);
             return a;
 
