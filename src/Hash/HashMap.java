7568e2952470f3e3d2fb78403d7674d0fd260194
Charmmy-ing <lc5207242@gmail.com>
2026-09-01 23:37:48 +0800

    9/1v1

--- src/Hash/HashMap.java
+++ src/Hash/HashMap.java
@@ -155,6 +155,52 @@ class Solution {
 //                }
 //                return true;
 //            }
+//        class Solution {
+//            public List<List<Integer>> fourSum(int[] nums, int target) {
+//                Arrays.sort(nums);
+//                List<List<Integer>> resultarr = new ArrayList<>();
+//                for (int i = 0; i < nums.length; i++) {
+//                    if (nums[i] > 0 && nums[i] >= target) {
+//                        break;
+//                    }
+//                    if (i > 0 && nums[i] == nums[i - 1]) {
+//                        continue;
+//                    }
+//
+//                    for (int k = i + 1; k < nums.length; k++) {
+//                        if (nums[i] + nums[k] > target && nums[i] + nums[k] >= 0) {
+//                            break;
+//                        }
+//                        if (k>i+1 &&nums[k] == nums[k - 1]) {
+//                            continue;
+//                        }
+//
+//                        int left = k + 1;
+//                        int right = nums.length - 1;
+//                        while (left < right) {
+//                            long sum = (long) nums[i] + nums[k] + nums[left] + nums[right];
+//                            if (sum > target) {
+//                                right--;
+//                            } else if (sum < target) {
+//                                left++;
+//                            } else {
+//                                resultarr.add(Arrays.asList(nums[i], nums[k], nums[left], nums[right]));
+//                                while (left < right && nums[right] == nums[right - 1]) {
+//                                    right--;
+//                                }
+//                                while (left < right && nums[left] == nums[left + 1]) {
+//                                    left++;
+//                                }
+//                                right--;
+//                                left++;
+//                            }
+//                        }
+//                    }
+//
+//                }
+//                return resultarr;
+//            }
+//        }
         }
     }
 
