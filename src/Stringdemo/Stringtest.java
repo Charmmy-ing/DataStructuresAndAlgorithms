0054fcc2b872f1b43d55f7f00d67174bd8c8a265
Charmmy-ing <lc5207242@gmail.com>
2026-09-02 08:40:46 +0800

    9/2v1

---
+++ src/Stringdemo/Stringtest.java
@@ -0,0 +1,28 @@
+package Stringdemo;
+
+public class Stringtest {
+    public static void main(String[] args) {
+      /*  class Solution {
+            public void reverseString(char[] s) {
+                int left=0;
+                int right=s.length-1;
+           while(left<right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
+        }
+
+                while(left<right){
+                    s[left]^=s[right];
+                    s[right]^=s[left];
+                    s[left]^=s[right];
+                    left++;
+                    right--;
+                }
+            }
+       */
+        }
+    }
+}
