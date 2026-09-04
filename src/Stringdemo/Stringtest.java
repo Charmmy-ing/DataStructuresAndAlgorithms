59ca9956dc4e5c13e10122e6e9df10405cac294f
Charmmy-ing <lc5207242@gmail.com>
2026-09-04 15:59:37 +0800

    9/4v3

--- src/Stringdemo/Stringtest.java
+++ src/Stringdemo/Stringtest.java
@@ -184,6 +184,43 @@ import java.util.*;
         }
     }
 }
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+        int[] next = new int[needle.length()];
+        getnext(needle, next);
+        int j = 0;
+        for (int i = 0; i < haystack.length(); i++) {
+            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                j = next[j - 1];
+            }
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == needle.length()) {
+                return i - needle.length() + 1;
+            }
+        }
+        return -1;
+
+    }
+
+    public static void getnext(String s, int[] next) {
+        int j = 0;
+        next[0] = 0;
+        for (int i = 1; i < s.length(); i++) {
+            while (j > 0 && s.charAt(j) != s.charAt(i)) {
+                j = next[j - 1];
+            }
+            if (s.charAt(j) == s.charAt(i)) {
+                j++;
+            }
+            next[i] = j;
+        }
+    }
+}
 
        */
 
