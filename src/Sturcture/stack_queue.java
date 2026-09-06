bc3140bc16c98249ff081758b53905cf6f29f790
Charmmy-ing <lc5207242@gmail.com>
2026-09-06 10:34:14 +0800

    9/6v2

--- src/Sturcture/stack_queue.java
+++ src/Sturcture/stack_queue.java
@@ -47,5 +47,51 @@ public class stack_queue {
  * int param_3 = obj.top();
  * boolean param_4 = obj.empty();
  */
+        /*class MyQueue {
+    Stack<Integer> stackIn;
+    Stack<Integer> stackout;
+
+    public MyQueue() {
+        stackIn = new Stack<Integer>();
+        stackout = new Stack<Integer>();
+    }
+
+    public void push(int x) {
+        stackIn.push(x);
+    }
+
+    public int pop() {
+        dumpStackIn();
+        return stackout.pop();
+    }
+
+    public int peek() {
+        dumpStackIn();
+        return stackout.peek();
+    }
+
+    public boolean empty() {
+        return stackIn.isEmpty() && stackout.isEmpty();
+    }
+
+    private void dumpStackIn() {
+        if (!stackout.isEmpty()) {
+            return;
+        }
+        while (!stackIn.isEmpty()) {
+            stackout.push(stackIn.pop());
+        }
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+
     }
 }
