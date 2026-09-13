8c8d321de999383294319f228e029869991742f1
Charmmy-ing <lc5207242@gmail.com>
2026-09-13 12:28:13 +0800

    9/13v1_今天写了层序遍历（bfs）的六道延伸题

--- src/TreeNode/Tree.java
+++ src/TreeNode/Tree.java
@@ -322,8 +322,289 @@ class Solution {
                 return result;
             }
         }
-        */
-        
+        /*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+        class Solution {
+            public List<List<Integer>> levelOrder(Node root) {
+                List<List<Integer>> result = new ArrayList<>();
+                Queue<Node> que = new LinkedList<>();
+                if (root == null) {
+                    return result;
+                }
+                que.offer(root);
+                while (!que.isEmpty()) {
+                    int len = que.size();
+                    List<Integer> list = new ArrayList();
+                    for (int i = 0; i < len; i++) {
+                        Node cur = que.poll();
+                        list.add(cur.val);
+                        List<Node> treeChild = new ArrayList<>();
+                        treeChild = cur.children;
+                        if (treeChild == null || treeChild.size() == 0) {
+                            continue;
+                        }
+                        for (Node child : treeChild) {
+                            que.offer(child);
+                        }
+                    }
+                    result.add(list);
+                }
+                return result;
+            }
+        }
+        /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+
+        class Solution {
+            public List<Integer> largestValues(TreeNode root) {
+                List<Integer> result = new ArrayList<>();
+                Queue<TreeNode> que = new LinkedList<>();
+                if (root == null) {
+                    return result;
+                }
+                que.add(root);
+                while (!que.isEmpty()) {
+                    int len=que.size();
+                    int max = Integer.MIN_VALUE;
+                    while (len > 0) {
+                        TreeNode cur = que.poll();
+                        max = Math.max(max, cur.val);
+                        if (cur.left != null) {
+                            que.add(cur.left);
+                        }
+                        if (cur.right != null) {
+                            que.add(cur.right);
+                        }
+                        len--;
+                    }
+                    result.add(max);
+                }
+                return result;
+            }
+        }
+        /*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+
+
+        class Solution {
+            public Node connect(Node root) {
+                Queue<Node> que = new LinkedList<>();
+                Node cur = new Node();
+                if (root == null) {
+                    return root;
+                }
+                que.add(root);
+                while (!que.isEmpty()) {
+                    int len = que.size();
+                    while (len > 0) {
+                        cur = que.poll();
+                        if (len == 1) {
+                            cur.next = null;
+                        } else {
+                            cur.next = que.peek();
+                        }
+                        if (cur.left != null) {
+                            que.add(cur.left);
+                        }
+                        if (cur.right != null) {
+                            que.add(cur.right);
+                        }
+                        len--;
+                    }
+                }
+                return root;
+            }
+        }
+        /*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+
+        class Solution {
+            public Node connect(Node root) {
+                Queue<Node> que = new LinkedList<>();
+                Node cur = new Node();
+                if (root == null) {
+                    return null;
+                }
+                que.add(root);
+                while (!que.isEmpty()) {
+                    int len = que.size();
+                    while (len > 0) {
+                        cur = que.poll();
+                        if (len == 1) {
+                            cur.next = null;
+                        } else {
+                            cur.next = que.peek();
+                        }
+                        if (cur.left != null) {
+                            que.add(cur.left);
+                        }
+                        if (cur.right != null) {
+                            que.add(cur.right);
+                        }
+                        len--;
+                    }
+                }
+                return root;
+            }
+        }
+        /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+
+        class Solution {
+            public int maxDepth(TreeNode root) {
+                Queue<TreeNode> que = new LinkedList<>();
+                int depth = 0;
+                if (root == null) {
+                    return depth;
+                }
+                que.add(root);
+
+                while (!que.isEmpty()) {
+                    int len = que.size();
+                    while (len > 0) {
+                        TreeNode cur = que.poll();
+                        if (cur.left != null) {
+                            que.add(cur.left);
+                        }
+                        if (cur.right != null) {
+                            que.add(cur.right);
+                        }
+                        len--;
+                    }
+                    depth++;
+                }
+                return depth;
+            }
+        }
+        /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+
+        class Solution {
+            public int minDepth(TreeNode root) {
+                Queue<TreeNode> que = new LinkedList<>();
+                int depth = 0;
+                if (root == null) {
+                    return depth;
+                }
+                que.add(root);
+
+                while (!que.isEmpty()) {
+                    int len = que.size();
+                    while (len > 0) {
+                        TreeNode cur = que.poll();
+                        if (cur.left == null && cur.right == null) {
+                            return depth + 1;
+                        }
+                        if (cur.left != null) {
+                            que.add(cur.left);
+                        }
+                        if (cur.right != null) {
+                            que.add(cur.right);
+                        }
+                        len--;
+                    }
+                    depth++;
+                }
+                return depth;
+            }
+        }
+
+         */
+
+
+
+
+
             }
 
 }
