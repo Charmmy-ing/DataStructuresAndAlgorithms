bef69836b43cf021ff0e8fdd6721ea027153c0e8
Charmmy-ing <lc5207242@gmail.com>
2026-09-16 17:27:22 +0800

    9/16v1_新写了一道反转二叉树的题，主要复习了几道题，三数之和，回文链表，层序遍历，递归遍历

--- src/TreeNode/Tree.java
+++ src/TreeNode/Tree.java
@@ -598,7 +598,48 @@ class Node {
                 return depth;
             }
         }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
 
+        class Solution {
+            public TreeNode invertTree(TreeNode root) {
+                if (root == null) {
+                    return null;
+                }
+                Queue<TreeNode> que = new LinkedList<>();
+                que.offer(root);
+                while (!que.isEmpty()) {
+                    TreeNode temp = que.poll();
+                    swap(temp);
+                    if (temp.left != null) {
+                        que.add(temp.left);
+                    }
+                    if (temp.right != null) {
+                        que.add(temp.right);
+                    }
+
+                }
+                return root;
+            }
+
+            public void swap(TreeNode root) {
+                TreeNode temp = root.left;
+                root.left = root.right;
+                root.right = temp;
+            }
+        }
          */
 
 
