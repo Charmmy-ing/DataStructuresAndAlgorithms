bc20c14ac93eb05dd636e61d591a695b55581ef2
Charmmy-ing <lc5207242@gmail.com>
2026-09-19 22:40:29 +0800

    9/19v1

--- src/TreeNode/Tree.java
+++ src/TreeNode/Tree.java
@@ -640,6 +640,29 @@ class Node {
                 root.right = temp;
             }
         }
+        class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftdepth = 0;
+        int rightdepth = 0;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        while (left != null) {
+            left = left.left;
+            leftdepth++;
+        }
+        while (right != null) {
+            right = right.right;
+            rightdepth++;
+        }
+        if (rightdepth == leftdepth) {
+            return (2 << leftdepth) - 1;
+        }
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+ 
          */
 
 
