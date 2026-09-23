c83cf6f2a986553276037ebebc96f798bba93c92
Charmmy-ing <lc5207242@gmail.com>
2026-09-23 22:36:53 +0800

    9/23v2

--- src/TreeNode/Tree.java
+++ src/TreeNode/Tree.java
@@ -722,8 +722,37 @@ class Node {
                 return res;
             }
         }
-*/
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ /*
+        class Solution {
+            public int sumOfLeftLeaves(TreeNode root) {
+                if (root == null) {
+                    return 0;
+                }
+                int leftnums = sumOfLeftLeaves(root.left);
+                int rightnums = sumOfLeftLeaves(root.right);
+                int nums = 0;
+                if (root.left != null && root.left.left == null && root.left.right == null) {
+                    nums = root.left.val;
+                }
+                return nums + leftnums + rightnums;
 
+            }
+        }
+*/
 
 
 
