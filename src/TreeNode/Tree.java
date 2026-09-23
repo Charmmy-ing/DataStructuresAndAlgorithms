5b471c0b650c1e17d81756a50522cd777f4c15e5
Charmmy-ing <lc5207242@gmail.com>
2026-09-23 22:35:54 +0800

    9/23v1

--- src/TreeNode/Tree.java
+++ src/TreeNode/Tree.java
@@ -684,7 +684,45 @@ class Node {
         return Math.max(leftHeight,rightHeight)+1;
     }
          */
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+/*
+        class Solution {
+            public int findBottomLeftValue(TreeNode root) {
+                Queue<TreeNode> que = new LinkedList<>();
+                que.add(root);
+                int res = 0;
+                while (!que.isEmpty()) {
+                    int len = que.size();
+                    for (int i = 0; i < len; i++) {
+                        TreeNode temp = que.poll();
+                        if (i == 0) {
+                            res = temp.val;
+                        }
+                        if (temp.left != null) {
+                            que.offer(temp.left);
+                        }
+                        if (temp.right != null) {
+                            que.offer(temp.right);
+                        }
+                    }
+                }
+                return res;
+            }
+        }
+*/
 
 
 
