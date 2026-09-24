d053d74deab89b40090aa3b20fbceaf0774ab8dc
Charmmy-ing <lc5207242@gmail.com>
2026-09-24 23:12:15 +0800

    9/24v2

--- src/TreeNode/Tree.java
+++ src/TreeNode/Tree.java
@@ -752,6 +752,49 @@ class Node {
 
             }
         }
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ *
+        class Solution {
+            public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+                List<List<Integer>> result = new ArrayList<>();
+                if (root == null) {
+                    return result;
+                }
+                List<Integer> list = new ArrayList<>();
+                pashdis(result, list, root, targetSum);
+                return result;
+            }
+
+            public void pashdis(List<List<Integer>> result, List<Integer> list, TreeNode root, int targetSum) {
+                list.add(root.val);
+                if (root.left == null && root.right == null) {
+                    if (targetSum - root.val == 0) {
+                        result.add(new ArrayList<Integer>(list));
+                    }
+                    return;
+                }
+                if (root.left != null) {
+                    pashdis(result, list, root.left, targetSum - root.val);
+                    list.remove(list.size() - 1);
+                }
+                if (root.right != null) {
+                    pashdis(result, list, root.right, targetSum - root.val);
+                    list.remove(list.size() - 1);
+                }
+            }
+        }
 */
 
 
